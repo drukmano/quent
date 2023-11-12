16e10f1033f57ecce38a16d4f29e68ba6055e316
Ohad Drukman <88836562+drukmano@users.noreply.github.com>
2023-11-13 00:26:32 +0200

    Update distribute.sh

--- scripts/distribute.sh
+++ scripts/distribute.sh
@@ -1,4 +1,8 @@
+rm -rf dist
+rm -rf build
+rm -rf src/quent.egg-info
+rm src/quent.cpython*
 python cython_setup.py build_ext --inplace
 python3 -m build
 rm dist/quent-*.whl
-python3 -m twine upload dist/*
+python3 -m twine upload dist/quent-*.tar.gz
